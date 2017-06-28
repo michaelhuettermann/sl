00bf89ddefe9be0d079989f69fee3110587165a6
michaelhuettermann <michael@huettermann.net>
2017-06-28 20:18:35 +0200

    adjust

--- src/com/huettermann/Util.groovy
+++ src/com/huettermann/Util.groovy
@@ -2,9 +2,11 @@
 package com.huettermann
 
 class Util {
-    static def setVersion(String pom) {
+    def setVersion(String pom) {
         echo "Calling shared library, with ${pom}."
         sh "mvn com.huettermann:versionfetcher:1.0.0:release versions:set -DgenerateBackupPoms=false -f ${pom}"
+    }
+    static void main(String []args){
     }
 }
 
