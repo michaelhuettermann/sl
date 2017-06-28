a3db70da39af4479425e4ddc7ae642915a925560
michaelhuettermann <michael@huettermann.net>
2017-06-28 19:27:15 +0200

    adjust

--- vars/releaseVersion.groovy
+++ vars/releaseVersion.groovy
@@ -1,4 +1,4 @@
-dev call(String name = 'pom.xml') {
-    echo "Calling shared library, with ${name}."
-    sh "mvn com.huettermann:versionfetcher:1.0.0:release versions:set -DgenerateBackupPoms=false -f ${name}"
+dev call(args) {
+    echo "Calling shared library, with ${args}."
+    sh "mvn com.huettermann:versionfetcher:1.0.0:release versions:set -DgenerateBackupPoms=false -f ${args}"
 }
