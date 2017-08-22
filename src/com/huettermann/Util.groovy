ad0b6ac1641be53f32b8092bb1f293f4857cefe1
michaelhuettermann <michael@huettermann.net>
2017-08-22 12:45:31 +0200

    some more info

--- src/com/huettermann/Util.groovy
+++ src/com/huettermann/Util.groovy
@@ -3,7 +3,7 @@ package com.huettermann
 
 class Util {
     def setVersion(String pom) {
-        echo "Calling shared library, with ${pom}."
+        echo "Calling shared library, a Util, with ${pom}."
         sh "mvn com.huettermann:versionfetcher:1.0.0:release versions:set -DgenerateBackupPoms=false -f ${pom}"
     }
     static void main(String []args){
--- vars/releaseVersion.groovy
+++ vars/releaseVersion.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/groovy
 def call(args) {
-    echo "Calling shared library, with ${args}."
+    echo "Calling shared library, from vars, with ${args}."
     sh "mvn com.huettermann:versionfetcher:1.0.0:release versions:set -DgenerateBackupPoms=false -f ${args}"
 }
