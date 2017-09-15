4cd3defc24e8e1d16d1a1ab7601844063dfaf8f0
michaelhuettermann <michael@huettermann.net>
2017-09-15 18:47:31 +0200

    new file

---
+++ vars/restCall.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/groovy
+import groovy.json.JsonSlurper;
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    retry(3){
+        def HttpURLConnection connection = config.url.openConnection()
+        if(config.BEARER.length() > 0)
+        {
+            def conn = config.url.openConnection()
+            connection.setRequestProperty("Authorization", "Bearer ${config.BEARER}")
+        }
+        connection.setRequestMethod("GET")
+        connection.setDoInput(true)
+
+        def pr = null
+        try {
+            connection.connect()
+            pr = new JsonSlurper().parse(new InputStreamReader(connection.getInputStream(),"UTF-8"))
+        } finally {
+            connection.disconnect()
+        }
+        return pr
+    }
+}
