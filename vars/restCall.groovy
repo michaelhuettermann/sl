b15a789ab46bd5fb3abf95b24e1022e952c99f93
michaelhuettermann <michael@huettermann.net>
2017-09-15 19:55:26 +0200

    a change

--- vars/restCall.groovy
+++ vars/restCall.groovy
@@ -2,7 +2,6 @@
 import groovy.json.JsonSlurper;
 
 def call(body) {
-    // evaluate the body block, and collect configuration into the object
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
@@ -13,17 +12,8 @@ def call(body) {
         println ( config.url )
         println ( config.bearer )
 
-
-
-        //def pr = "curl -sk -X GET -H \"Authorization: Bearer ${config.bearer} \"${config.url}\""
-
-
         def command = "curl -sk -X \"GET\" -H \"Authorization: Bearer ${config.bearer}\" \"${config.url}\""
-
-
         println ( command )
-
-        println  " ... "
 
         def proc = command.execute()
         proc.waitFor()
@@ -32,26 +22,6 @@ def call(body) {
         println "Std Err: ${proc.err.text}"
         println "Std Out: ${proc.in.text}"
 
-
-        /*
-        def HttpURLConnection connection = config.url.toURL().openConnection()
-
-        if(config.bearer.length() > 0)
-        {
-            def conn = config.url.openConnection()
-            connection.setRequestProperty("Authorization", "Bearer ${config.bearer}")
-        }
-        connection.setRequestMethod("GET")
-        connection.setDoInput(true)
-
-        def pr = null
-        try {
-            connection.connect()
-            pr = new JsonSlurper().parse(new InputStreamReader(connection.getInputStream(),"UTF-8"))
-        } finally {
-            connection.disconnect()
-        }
-        */
-        return null
+        return ${proc.in.text}
     }
 }
